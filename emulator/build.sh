64efd4e8219a1ed92885d323a2e5a69000ffc92c
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2024-10-21 05:02:25 -0300

    add emulator base

---
+++ emulator/build.sh
@@ -0,0 +1,2 @@
+g++ ./src/main.cpp -o ./bin/hkcpu-emulator
+chmod +x ./bin/hkcpu-emulator
---
+++ emulator/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "emulator\n";
+    return 0;
+}
