f7dfddeb2b865a9ffe6dbe4bfa3561cf5a6dd5fd
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2025-04-26 00:06:56 -0300

    comment emulator main and fileMgr files

--- emulator/src/fileMgr.h
+++ emulator/src/fileMgr.h
@@ -1,48 +1,48 @@
 #pragma once
 
 void loadRom(int argc, char *argv[]) {
-    if(argc <= 1) {
+    if(argc <= 1) { // detect if the command hasn't some argument
         cout << "add input rom, exemple:\n./hkcpu-emulator ./memory.rom\n";
         exit(1);
     }
     cout << "hkcpu emulator - 8bits CPU\n";
-    string romfile = argv[1];
-    ifstream arq(romfile, ios::binary | ios::ate);
-    if(!arq) {
+    string romfile = argv[1]; // define romfile as fist argument
+    ifstream arq(romfile, ios::binary | ios::ate); // open the file correponding to the first argument
+    if(!arq) { // if has some error opening file
         cout << "can't open: " << romfile << "\n";
         exit(1);
     }
-    streamsize romSize = arq.tellg();
-    if(romSize != sizeof(ROM)) {
+    streamsize romSize = arq.tellg(); // sets the rom size
+    if(romSize != sizeof(ROM)) { // error if rom size is diferrent of the program ROM
         cout << "Error: ROM file size is " << romSize << "B. Expected size is 256B.\n";
         exit(1);
     }
     arq.seekg(0, ios::beg);
-    arq.read(reinterpret_cast<char*>(ROM), sizeof(ROM));
-    if(arq.gcount() != sizeof(ROM)) {
+    arq.read(reinterpret_cast<char*>(ROM), sizeof(ROM)); // read the file of rom
+    if(arq.gcount() != sizeof(ROM)) { // if the bytes count did read if diferrent of ROM size, then has some error
         cout << "Error reading: " << romfile << "\n";
         exit(1);
     }
-    arq.close();
+    arq.close(); // close the file
     cout << "ROM loaded.\n";
 }
 
 void saveRom(int argc, char *argv[]) {
-    if(argc <= 2) {
+    if(argc <= 2) { // exit if don't has arguments
         cout << "add output rom, exemple:\n./hkcpu-emulator ./input.rom ./output.rom\n";
         exit(1);
     }
-    string romfile = argv[2];
-    ofstream arq(romfile, ios::binary);
-    if(!arq) {
+    string romfile = argv[2]; // get the output file name
+    ofstream arq(romfile, ios::binary); // open the output file
+    if(!arq) { // if has some error opening the file
         cout << "can't open or create: " << romfile << "\n";
         exit(1);
     }
-    arq.write(reinterpret_cast<char*>(ROM), sizeof(ROM));
-    if (!arq) {
+    arq.write(reinterpret_cast<char*>(ROM), sizeof(ROM)); // write ROM on file
+    if (!arq) { // see for errors while writing
         cout << "Error writing to: " << romfile << "\n";
         exit(1);
     }
-    arq.close();
+    arq.close(); // close and return
     cout << "ROM saved.\n";
 }
--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -12,23 +12,24 @@ u_int8_t ROM[256];
 #include "./fileMgr.h"
 
 int main(int argc, char *argv[]) {
-    CPU core;
+    CPU core; // define core as a CPU object
     
-    loadRom(argc, argv);
-    core.setRam(ROM);
+    loadRom(argc, argv); // load the ROM from arguments
+    core.setRam(ROM); // load ROM to core RAM
     
     bool hlt;
     int clockCount;
-    while(!hlt) {
+    while(!hlt) { // cpu cicle
         if(clockCount == 10) {
+            // flip the clock state every 10 cpu cicles
             clockCount = 0;
             core.flipClock();
         }
-        if(core.propagate() == 2) hlt = 1;
+        if(core.propagate() == 2) hlt = 1; // propag CPU, if propagate return 2 (hlt) set hlt to 1 to stop the while.
         clockCount++;
     }
     cout << "hlt";
-    core.getRam(ROM);
-    saveRom(argc, argv);
+    core.getRam(ROM); // load the core RAM to ROM
+    saveRom(argc, argv); // save the result ROM file
     return 0;
 }
